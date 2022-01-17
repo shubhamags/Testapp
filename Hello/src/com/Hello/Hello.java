8a15fb0d7e6f9c4e7832488b60a50782e92b6927
shubhambhardwaj <shubhambhardwaj@EZDI-LAP0108>
2022-01-17 16:39:40 +0530

    Java program to find sum of an array.

---
+++ Hello/src/com/Hello/Hello.java
@@ -0,0 +1,18 @@
+package com.Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		  int arr[] = {1,2,3,4,5,6,7};
+		  int sum = 0;
+		  for(int i=0;i<arr.length;i++)
+		  {
+			  sum = sum + arr[i];
+		  }
+		 System.out.print("sum of array is:"+ sum);
+
+	}
+	
+	
+
+}
